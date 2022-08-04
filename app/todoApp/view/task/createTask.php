b6fc71dd8c0d31c9d550318a4139d97e5110a16f
olesia <o.morozova@e2e4online.ru>
2022-08-04 10:02:07 +0700

    todolist

--- app/todoApp/view/task/createTask.php
+++ app/todoApp/view/task/createTask.php
@@ -1,27 +1,27 @@
 <h2>Create task</h2>
 <form method="post">
-	<input type="hidden" name="id">
-	<div class="id-parent">
-    	<div class="id-child">
-			<table class="table table-sm form-table">
-		     	<tbody>
-		     		<tr>
-			          <td>Name:</td>
-			          <td><input type="text" name="name"></td>
-			        </tr>
-			        <tr>
-			          <td>Email:</td>
-			          <td><input type="email" name="email"></td>
-			        </tr>
-			       	<tr>
-			          <td>Task description:</td>
-			          <td><input type="text" name="description"></td>
-			        </tr>
-				  </tbody>
-			</table>
-		</div>
-	</div>
-	<input class="btn btn-secondary" type="submit" name="createTask" value="Сохранить">
+    <input type="hidden" name="id">
+    <div class="id-parent">
+        <div class="id-child">
+            <table class="table table-sm form-table">
+                <tbody>
+                <tr>
+                    <td>Name:</td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><input type="email" name="email"></td>
+                </tr>
+                <tr>
+                    <td>Task description:</td>
+                    <td><input type="text" name="description"></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <input class="btn btn-secondary" type="submit" name="createTask" value="Сохранить">
 </form>
 
 <span id='message'>
